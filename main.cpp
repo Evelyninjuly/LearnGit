8520eb908e1a87b960cd0391a95124fada6e06b5
PerfecterDay <perfect_a_day@163.com>
2015-06-18 20:24:40 +0800

    1

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/**
+**author :PerfectDay **
+**    **
+**--题**
+**2015年 月 日**
+**/
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdio>
+#include <queue>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+
+}
